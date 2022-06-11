83f15fc37a27f40bcce390cdbfd4234b71616e5f
unknown <supratimhazarika78@gmail.com>
2022-06-11 09:38:44 +0530

    image issue

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,8 +15,8 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Image
-          src="/../public/assets/logo.png"
+        <img
+          src="../public/assets/logo.png"
           alt="/"
           width="50"
           height="50"
